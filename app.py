6e2871f39d848cbd7e64251cf6f9eb43c1b34a6a
Tyler M. Neher <tyler@neherdata.com>
2025-09-14 16:31:18 -0400

    names
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
+@app.route("/", methods=["GET", "POST"])
+def index():
+    names = None
+    if request.method == "POST":
+        raw = request.form.get("names", "")
+        # Split on commas, strip whitespace, ignore empty entries
+        names = [n.strip() for n in raw.split(",") if n.strip()]
+    return render_template("index.html", names=names)
 
 
-@app.route('/startgame', methods=['POST', 'GET'])
-def startgame():
-    if request.method == 'POST':
-        user = request.form['player']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('player')
-        return redirect(url_for('success', name=user))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
